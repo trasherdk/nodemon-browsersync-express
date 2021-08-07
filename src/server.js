e9cbe2c2ebe1acd45ce6c7e9a6c7c31f92a5b587
TrasherDK <trasherdk@fumlersoft.dk>
2021-08-07 22:02:43 +0200

    Refactor `koa` to `express`

--- src/server.js
+++ src/server.js
@@ -1,19 +1,16 @@
-const Koa = require('koa')
-const Router = require('koa-router')
-const cors = require('kcors')
+const express = require('express')
+const cors = require('cors')
 
-const app = new Koa()
-const router = new Router()
+const app = new express()
+const router = express.Router()
 
-router.get(`/`, ctx => {
-  ctx.body = {
+router.get(`/`, async (req, res, next) => {
+  res.status(200).send({
     dogs: [`Fido`, `Rover`, `Pussycat`, `Dude`]
-  }
+  })
 })
 
-app
-  .use(cors())
-  .use(router.routes())
-  .use(router.allowedMethods())
+app.use(cors())
+app.use(router)
 
-app.listen(1337, () => console.log(`Koa at: http://localhost:1337`))
+app.listen(1337, '127.0.0.1', () => console.log(`Express at: http://localhost:1337`))
